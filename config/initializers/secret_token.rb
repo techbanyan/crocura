3aa2d226d205a35cfaedb026ae12a32d5322e1a6
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-07-23 14:09:42 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crocura::Application.config.secret_token = '3b26f846385ce70ff4c9ca70a37f5ac9095d65afc836795dc3b3b41268bb358e5810ce57b2f4e0514fcf3fef0c9321269c3d8faa85b760162629eecfcf2235f8'
