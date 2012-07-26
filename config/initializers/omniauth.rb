0b5087716ae6f67c2167df2e899052596d8df4be
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-07-26 19:02:51 +0530

    middle header done

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	raise "#{instagram_credentials_file} is missing!" unless File.exists? instagram_credentials_file
 	instagram_credentials = YAML.load_file(instagram_credentials_file)[Rails.env].symbolize_keys
 
-  	provider :instagram, instagram_credentials[:CLIENT_ID], instagram_credentials[:CLIENT_SECRET]
+  	provider :instagram, instagram_credentials[:CLIENT_ID], instagram_credentials[:CLIENT_SECRET], {:scope => 'likes comments'}
 
 	OmniAuth.config.on_failure = SessionsController.action(:oauth_failure)
 end
