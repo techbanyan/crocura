9f2881b1cbb087e844e56ad6e6320779081abaff
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-08-01 22:27:48 +0530

    duplicate rendering fixed

--- app/assets/javascripts/stream.js
+++ app/assets/javascripts/stream.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-
+	
 	$('.load-more')
+
 	    .live("ajax:beforeSend", function(evt, xhr, settings){
 	      var $submitButton = $(this).find('input[name="commit"]');
 	      $(this).text("LOADING ...");
@@ -8,7 +9,8 @@ $(document).ready(function(){
 
 		.live("ajax:success", function(evt, data, status, xhr){
 			$(xhr.responseText).appendTo('.get-more-stream');
-	      	$(document.body).animate({scrollTop: $('.get-more-stream').children().last().offset().top-200}, 1000);
+	      	$('html,body').animate({scrollTop: $('.get-more-stream').children().last().offset().top-200}, 1000);
 	      	$(this).text("SHOW MORE");
 	    })
 });
+
