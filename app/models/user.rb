5437c55d754eb9c65dde450f21bb1607b3600f05
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-08-08 17:43:35 +0530

    tweaking username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :provider, :uid
+  attr_accessible :name, :provider, :uid, :username
 
   def self.create_with_omniauth(auth)
   	create! do |user|
   		user.provider = auth["provider"]
   		user.uid = auth["uid"]
   		user.name = auth["info"]["name"]
-  		user.username = auth["info"]["nickname"]
+  		user.username = auth["info"]["name"]
   	end
   end
 end
