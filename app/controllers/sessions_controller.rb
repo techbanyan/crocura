2de53a3ac07eea1d84a6b46667a7ce3ff4d3db1a
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-08-08 18:06:37 +0530

    tweaking username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
 
 	def destroy
 		session[:user_id] = nil
+		session[:access_token] = nil
 		redirect_to root_url
 		flash[:success] = "Bye!"
 	end
