f665627f0c65ac619c682162fcdbf44725ca4799
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-08-22 16:34:13 +0530

    admin

--- app/admin/tabs.rb
+++ app/admin/tabs.rb
@@ -4,8 +4,6 @@ ActiveAdmin.register Tab do
         f.inputs "Tab Details" do
             f.input :query
 		        f.input :query_type, :as => :select, :collection => ["tag", "location"], :include_blank => false
-		        f.input :latitude
-		        f.input :longitude
 		        f.input :rank
         end
         f.buttons
