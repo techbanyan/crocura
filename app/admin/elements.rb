cc13d7668a0a87f36d2e2c293c0749c19a52c070
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-08-23 16:34:51 +0530

    rich text editor in admin for faq, privacy, help, about, etc.

--- app/admin/elements.rb
+++ app/admin/elements.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Element do
-
+    config.clear_action_items!
     form do |f|
         f.inputs "Element Details" do
             f.input :privacy, :as => :rich
@@ -9,5 +9,19 @@ ActiveAdmin.register Element do
             f.input :about, :as => :rich
         end
         f.buttons
-    end  
+    end
+
+    index do
+        column :privacy
+        column :help
+        column :faq
+        column :how_it_works
+        column :about
+        column "Actions" do |element|
+            link_to "View", admin_element_path(element)
+        end
+        column "Actions" do |element|
+            link_to "Edit", edit_admin_element_path(element)
+        end
+    end
 end
