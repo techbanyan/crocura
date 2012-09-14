a5a0c5bc174b340166c403bf2d45d7890fc98a9c
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-09-14 21:03:08 +0530

    login issue fixes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Crocura::Application.initialize!
+
+Rails::Initializer.run do |config|
+  config.middleware.use "NoWWW" if RAILS_ENV == "production"
+end
