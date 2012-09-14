734c1a671630f297507ef53e462081beccde772b
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-09-14 21:07:13 +0530

    login issue fixes

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ end
 module Crocura
   class Application < Rails::Application
 
+    config.middleware.use "NoWWW" if RAILS_ENV == "production"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Crocura::Application.initialize!
-
-Rails::Initializer.run do |config|
-  config.middleware.use "NoWWW" if RAILS_ENV == "production"
-end
